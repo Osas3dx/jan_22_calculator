48267557eb1b093efa4492fdac2e492c7c44e78c
Marc Gibbons <1726961+marcgibbons@users.noreply.github.com>
2019-02-05 19:47:08 -0500

    Build a divide function for our calculator

--- calculator.py
+++ calculator.py
@@ -1,8 +1,31 @@
 """
 A calculator library.
 """
+import math
+
 def add(*args):
     """
     add() Returns the sum of n-parameters.
     """
     return sum(args)
+
+
+def divide(*args):
+    """
+    Returns the result of the division of n-parameters.
+    """
+    if not args:
+        return math.nan
+    if len(args) == 1:
+        return 1
+
+    args = list(args)
+    answer = args.pop(0)
+
+    for value in args:
+        try:
+            answer /= value
+        except ZeroDivisionError:
+            return math.nan
+
+    return answer
